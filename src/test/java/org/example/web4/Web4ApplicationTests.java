d76c6eeb85fd29bc628f030dc855928a1e349e27
chousik <Zaxap2018@yandex.ru>
2025-04-20 22:51:57 +0300

    need history, test

--- src/test/java/org/example/web4/Web4ApplicationTests.java
+++ src/test/java/org/example/web4/Web4ApplicationTests.java
@@ -1,13 +1,13 @@
 package org.example.web4;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class Web4ApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals(3, 4);
     }
 
 }
