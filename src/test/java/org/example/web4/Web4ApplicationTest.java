964f054dac60d4b6778ebd63cfb6183768c9ce3e
chousik <Zaxap2018@yandex.ru>
2025-04-20 23:13:39 +0300

    test ready

--- src/test/java/org/example/web4/Web4ApplicationTest.java
+++ src/test/java/org/example/web4/Web4ApplicationTest.java
@@ -3,7 +3,7 @@ package org.example.web4;
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
 
-public class Web4ApplicationTests {
+public class Web4ApplicationTest {
     @Test
     public void contextLoads() {
         assertEquals(3, 4);
